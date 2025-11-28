65f968e3dc6bbfc27861d604c0c99af4b2632adc
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-11-28 12:56:11 +0700

    membuat interface untuk mengambil data siswa dan menambah siswa, lalu menyediakan implementasi offline yang menjalankan perintah itu lewat SiswaDAO, sehingga repository hanya meneruskan tugas ke DAO

--- app/src/main/java/com/example/praktikum7/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/praktikum7/repositori/RepositoriSiswa.kt
@@ -1,7 +1,7 @@
 package com.example.praktikum7.repositori
 
 import com.example.praktikum7.room.Siswa
-import com.example.praktikum7.room.SiswaDao
+import com.example.praktikum7.room.SiswaDAO
 import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa {
@@ -10,7 +10,7 @@ interface RepositoriSiswa {
 }
 
 class OfflineRepositoriSiswa(
-    private val siswaDao: SiswaDao
+    private val siswaDao: SiswaDAO
 ): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun InsertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
