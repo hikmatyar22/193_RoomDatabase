0f53a5acae608cabde20ab506b9abba4d373e3e0
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-11-28 12:58:33 +0700

    membuat Interface SiswaDAO ini berisi fungsi untuk mengambil semua data siswa secara terurut (A–Z) menggunakan Flow, dan fungsi untuk menambahkan data siswa baru ke tabel dengan mengabaikan data duplikat.

--- app/src/main/java/com/example/praktikum7/room/SiswaDao.kt
+++ app/src/main/java/com/example/praktikum7/room/SiswaDao.kt
@@ -1,11 +1,13 @@
 package com.example.praktikum7.room
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
-interface SiswaDao {
+@Dao
+interface SiswaDAO {
     @Query("SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa() : Flow<List<Siswa>>
 
