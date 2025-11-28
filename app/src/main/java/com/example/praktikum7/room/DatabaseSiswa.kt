cc08cab6e5a76915d1d497e14bc731720f2d34f6
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-11-28 12:56:46 +0700

    membuat database Room bernama DatabaseSiswa yang berisi tabel Siswa dan menyediakan akses ke SiswaDAO; fungsi getDatabase memastikan aplikasi hanya membuat satu instance database secara aman, dan akan membuatnya jika belum ada.

--- app/src/main/java/com/example/praktikum7/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/praktikum7/room/DatabaseSiswa.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa::class], version =  1, exportSchema = false)
 abstract class DatabaseSiswa : RoomDatabase(){
-    abstract fun siswaDao() : SiswaDao
+    abstract fun siswaDao() : SiswaDAO
 
     companion object {
         @Volatile
