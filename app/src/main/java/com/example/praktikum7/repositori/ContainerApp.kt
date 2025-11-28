6f20380e08fede2b63eac5d14a797a661f024fb8
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-11-28 10:14:49 +0700

    membuat interface containerAPP dan membuat wadah (container) yang menyediakan satu objek variabel val repositoriSiswa dimana repository itu dibuat untuk mengambil DAO dari database, sehingga aplikasi punya satu tempat khusus untuk mengakses repository siswa.

---
+++ app/src/main/java/com/example/praktikum7/repositori/ContainerApp.kt
@@ -0,0 +1,18 @@
+package com.example.praktikum7.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.praktikum7.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
+
