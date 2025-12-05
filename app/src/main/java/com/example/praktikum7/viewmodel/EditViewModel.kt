4e08175d6852cc6431186b6b2388049510e66737
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-12-05 10:27:38 +0700

    membuat  ViewModel untuk halaman edit siswa yang: mengambil idSiswa dari SavedStateHandle, memuat data siswa awal ke uiStateSiswa, menyediakan fungsi updateUIState untuk memperbarui state, memvalidasi input, dan fungsi updateSiswa() untuk menyimpan perubahan siswa secara asinkron.

---
+++ app/src/main/java/com/example/praktikum7/viewmodel/EditViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.praktikum7.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.praktikum7.repositori.RepositoriSiswa
+import com.example.praktikum7.view.route.DestinasiEditSiswa
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int =
+        checkNotNull(savedStateHandle[DestinasiEditSiswa.itemIdArg])
+
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoriSiswa.getSiswaStream(idSiswa)
+                .filterNotNull()
+                .first()
+                .toUiStateSiswa(true)
+        }
+    }
+
+    fun updateUIState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            uiStateSiswa.copy(
+                detailSiswa = detailSiswa,
+                isEntryValid = validasiInput(detailSiswa)
+            )
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    suspend fun updateSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)) {
+            repositoriSiswa.updateSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
