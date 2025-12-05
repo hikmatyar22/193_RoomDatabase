0a4cc24024a0ceffdac14c44bb4f3cb5a375fa93
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-12-05 09:22:11 +0700

    membuat dua fungsi dengan memanggil siswaDao secara asinkron untuk menghapus atau memperbarui data siswa di database.

--- app/src/main/java/com/example/praktikum7/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/praktikum7/repositori/RepositoriSiswa.kt
@@ -22,5 +22,6 @@ class OfflineRepositoriSiswa(
 
     //tambahan
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
-
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
