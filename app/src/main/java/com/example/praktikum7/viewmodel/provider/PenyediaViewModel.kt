2d56a81bbee5c306a8b06d8285d0492682f13092
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-12-05 10:33:02 +0700

    modifikasi dan menambahkan initializer pada ViewModelProvider.Factory untuk membuat instance DetailViewModel dan EditViewModel, masing-masing dengan SavedStateHandle dan RepositoriSiswa dari aplikasi, agar bisa digunakan di Compose atau Activity/Fragment.

--- app/src/main/java/com/example/praktikum7/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum7/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.praktikum7.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.praktikum7.repositori.AplikasiSiswa
+import com.example.praktikum7.viewmodel.DetailViewModel
+import com.example.praktikum7.viewmodel.EditViewModel
 import com.example.praktikum7.viewmodel.EntryViewModel
 import com.example.praktikum7.viewmodel.HomeViewModel
 
@@ -15,6 +18,19 @@ object PenyediaViewModel{
         }
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        //Tambaahan
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
         }
     }
 }
