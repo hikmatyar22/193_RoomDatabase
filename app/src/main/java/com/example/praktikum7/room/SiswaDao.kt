570880ad4ff7fe1b5beee2581b3a19dc02c40a13
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-12-05 10:30:09 +0700

    modifikasi siswa dao di room

--- app/src/main/java/com/example/praktikum7/room/SiswaDao.kt
+++ app/src/main/java/com/example/praktikum7/room/SiswaDao.kt
@@ -9,20 +9,20 @@ import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface SiswaDAO {
-    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
+interface SiswaDao {
+    @Query(value = "SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa() : Flow<List<Siswa>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa : Siswa)
 
-    //Tambahan 5/12/2025
+    //EDIT : Tambah fungsi
     @Query("SELECT * from tblSiswa WHERE id = :id")
-    fun getSiswa(id: Int) : Flow<Siswa>
+    fun getSiswa(id: Int): Flow<Siswa?>
 
     @Delete
-    suspend fun delete(siswa: Siswa)
+    suspend fun delete (siswa: Siswa)
 
     @Update
-    suspend fun update(siswa: Siswa)
+    suspend fun update (siswa: Siswa)
 }
